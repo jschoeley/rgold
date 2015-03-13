ae686f0663be2354a4dfb820031ca55a6d1ef8a3
Jonas Schöley <j.schoeley@gmail.com>
2015-03-13 14:37:12 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+`rgold`: Useful `R` Packages
+============================
+
+Just a list of `R` library calls I use in various projects.
---
+++ rgold.R
@@ -0,0 +1,34 @@
+#### Useful R packages
+
+# visualization
+library(ggplot2)    # 2d plotting framework
+library(gtable)     # work with ggplot objects
+library(ggvis)      # interactive 2d plotting
+library(ggtern)     # ternary diagrams
+library(grid)       # basic graphing tools
+library(gridBase)   # integrating grid and base graphics
+library(gridExtra)  # extended grid functions
+library(scales)     # different scales, RGB-alpha functions
+library(colorspace) # handle different colour-spaces
+
+# data transformation
+library(tidyr)      # tidy data, convert between long and wide
+library(dplyr)      # data verbs, operations on subsets of data
+
+# literate programming
+library(xtable)     # latex and html tables
+
+# optimization
+library(DEoptim)    # differential evolution algorithm
+
+# computing
+library(doParallel) # parallel computing back-end
+library(foreach)    # parallel for loop
+library(Rmpfr)      # arbitrarily accurate computation
+
+# survival analysis
+library(survival)   # survival analysis
+
+# misc
+library(extrafont)  # access fonts on system
+library(devtools)   # developer tools, github interface
